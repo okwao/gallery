935e5aea90e44ea120c27585586d8750f2ae3813
Laud Bentil <laud.bentil@meltwater.org>
2015-03-16 22:15:34 +0000

    basic changes to image.js, artists.js & router.js

--- both/artists.js
+++ both/artists.js
@@ -1,1 +1,13 @@
 Artists = new Mongo.Collection('artists');
+
+Artists.attachSchema(new SimpleSchema({
+  description:{
+    type: String,
+    max: 300,
+    label: 'The Story',
+    
+    autoform:{
+      rows:3
+    }
+  }
+}));
---
+++ both/router.js
@@ -0,0 +1,57 @@
+/*
+router style, initial configuration template
+layoutTemplate will map to a layout template
+notFoundTemplate will use a template named 404 or a name of your choice if you chose to change it
+loadingTemplate will use a template with loading or a template
+
+*/
+Router.configure({
+  layoutTemplate: 'layout',
+  notFoundTemplate: '404',
+  loadingTemplate: 'loading'
+});
+
+// ============================================== routes ========================================================================
+
+//  route one basic
+Router.route ('/', function(){
+  this.render('home');
+  name: 'home'
+});
+
+// // with params sent from the browser
+// Router.route('/users/:_id', function(){
+//   this.render('user');
+//   name: 'userDetails'
+//   },{
+//     data: function() {
+//      var id = this.params._id;
+//      return {
+//        user: Users.findOne(id);
+//      }
+//   }
+// });
+
+// // for different loading template and on wait
+
+// Router.map( function () {
+//   this.route('user', {
+//     path: '/users/details/:_id',
+//     name: 'userDetail',
+//     layoutTemplate: 'userLayout',
+
+//     waitOn: function(){
+//       var pass = this.params._id;
+//       return [
+//         Meteor.subscribe('users', pass)      ]
+//     },
+
+//     data: function() {
+//       return {
+//         info: Collections.find()
+//       }
+//     }
+//   });
+// });
+
+//     
