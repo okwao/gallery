da2e4984b3e1bfe8f138c2d534f4f0056334573a
Laud Bentil <laud.bentil@meltwater.org>
2015-03-18 10:06:51 +0000

    added details to contact page

--- both/artists.js
+++ both/artists.js
@@ -1,13 +1,27 @@
 // Artists = new Mongo.Collection('artists');
 
 // Artists.attachSchema(new SimpleSchema({
+//   name: {
+//     type: String,
+//     max: 100,
+//     label: 'Name'
+//   },
 //   description:{
 //     type: String,
 //     max: 300,
-//     label: 'The Story',
-    
+//     label: 'A little about Me',
 //     autoform:{
 //       rows:3
 //     }
-//   }
+//   },
+//   gallerypicture: {
+//    type: String,
+//    autoform: {
+//      afFieldInput: {
+//        type: 'fileUpload',
+//        collection: 'Images'
+//      }
+//    },
+//    label: 'Upload a Gallery Image'
+//  }
 // }));
---
+++ both/gallery.js
@@ -0,0 +1,16 @@
+// Gallery = new Mongo.Collection('gallery');
+
+// Gallery.attachSchema(new SimpleSchema({
+//   owner: {
+//     type: String,
+//     autoform: {
+//       omit: true
+//     },
+//     autoValue: function(){
+//       if(this.isInsert){
+//         return Meteor.userId();
+//       }
+//     }
+//   }
+// }));
+
